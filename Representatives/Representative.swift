fcf1452edb719bd79132daf04e959410e0937695
Thang Tong <tongthang0104@gmail.com>
2015-10-29 14:45:14 -0600

    Reprenstative.swift and pickerview

---
+++ Representatives/Representative.swift
@@ -0,0 +1,43 @@
+//
+//  Representative.swift
+//  Representatives
+//
+//  Created by Thang H Tong on 10/29/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Representative {
+    
+    static let kNameKey = "name"
+    static let kPartyKey = "party"
+    static let kStateKey = "state"
+    static let kDistrictKey = "district"
+    static let kPhoneKey = "phone"
+    static let kOfficeKey = "office"
+    static let kLinkKey = "link"
+    static let kResultsKey = "results"
+    
+    var name: String?
+    var party: String?
+    var state: String?
+    var district: String?
+    var phone: String?
+    var office: String?
+    var link: String?
+    
+    init(jsonDictionary: [String: String]) {
+        self.name = jsonDictionary[Representative.kNameKey]
+        self.party = jsonDictionary[Representative.kPartyKey]
+        self.state = jsonDictionary[Representative.kStateKey]
+        self.district = jsonDictionary[Representative.kDistrictKey]
+        self.phone = jsonDictionary[Representative.kPhoneKey]
+        self.office = jsonDictionary[Representative.kOfficeKey]
+        self.link = jsonDictionary[Representative.kLinkKey]
+    }
+    
+        
+        
+    
+}
--- Representatives/SearchViewController.swift
+++ Representatives/SearchViewController.swift
@@ -8,9 +8,31 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     //MARK: - Properties
     let states = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        return states[row]
+        
+    }
+    
+   
+    @IBAction func searchButtonTapped(sender: UIButton) {
+    }
+    
+    
     
 }
